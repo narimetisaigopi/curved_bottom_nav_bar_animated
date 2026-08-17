306ad178f5cde5dd714a50c78f5095c95558c555
narimetisaigopi <narimetisaigopi@gmail.com>
2026-08-17 14:51:04 +0530

    added documentation images

---
+++ lib/curved_bottom_nav_bar.dart
@@ -0,0 +1,6 @@
+/// A customizable curved bottom navigation bar with an animated floating
+/// bubble that highlights the active tab.
+library curved_bottom_nav_bar_animated;
+
+export 'src/curved_bottom_nav_bar.dart';
+export 'src/curved_bottom_nav_item.dart';
